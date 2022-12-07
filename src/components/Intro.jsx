cc34dd117d33ef9a775afe4836e3cf5cfdc4e566
Shubham R Pawar <mjshubhampawar21@gmail.com>
2022-12-07 16:30:26 +0530

    intro text

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Intro() {
-  return <div></div>;
+  return (
+    <>
+      <main>
+        <section id="intro" className="hero">
+          <div className="heroText">
+            <p className="topData">Hey, I'm</p>
+            <h1 className="title">Shubham Pawar</h1>
+            <p className="heroData">I'm a MERN stack Dev</p>
+          </div>
+          <div className="heroImg"></div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default Intro;
