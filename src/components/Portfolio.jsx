39f1a920eda0d7eae70a7b7ec03a3ffd79cb757f
Shubham R Pawar <mjshubhampawar21@gmail.com>
2022-12-12 18:44:46 +0530

    Fixxed Card and added git link.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,10 +5,16 @@ function Portfolio() {
     <>
       <div className="projects"> My Projects.</div>
       <section class="page-contain">
-        <a href="#" class="data-card">
+        <a href="#projects" class="data-card">
           <h3>01</h3>
           <h4>Portfolio Site.</h4>
           <p>My personal site using ReactJs.</p>
+          <a
+            href="https://github.com/mjshubham21/Mjshubham21Portfolio"
+            target="_blank"
+          >
+            <button className="btn">GitHub</button>{" "}
+          </a>
         </a>
       </section>
     </>
