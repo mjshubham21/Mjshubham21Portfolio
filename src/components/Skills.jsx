a457768b4d0e8d08504fa987f5a837c0b1ddcf64
Shubham R Pawar <mjshubhampawar21@gmail.com>
2022-12-16 13:55:53 +0530

    fontawesome err.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
 function Skills() {
   return (
     <>
-      <section className="skills"></section>
+      <section className="skills">
+        <div className="card">
+          <div className="content">
+            <FontAwesomeIcon icon="fa-brands fa-html5" />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
