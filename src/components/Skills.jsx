8c256f9b19a97308cab741d32a895f9a2a142e78
Shubham R Pawar <mjshubhampawar21@gmail.com>
2022-12-18 14:39:22 +0530

    Some new Icon errs, not fixed!!

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { HtmlIcon, CssIcon, JavascriptIcon } from "@mui/icons-material";
 function Skills() {
   return (
     <>
       <section className="skills">
         <div className="card">
           <div className="content">
-            <FontAwesomeIcon icon="fa-brands fa-html5" />
+            {/* <FontAwesomeIcon icon="fa-brands fa-html5" /> */}
+            <HtmlIcon />
+            <CssIcon />
+            <JavascriptIcon />
           </div>
         </div>
       </section>
