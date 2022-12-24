1f9eddecd20691021eb5fb061cde8025d13b26aa
Shubham R Pawar <mjshubhampawar21@gmail.com>
2022-12-24 17:25:49 +0530

    Footer 1st iteration.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 function Footer() {
-  return <></>;
+  return (
+    <>
+      <footer class="footer-distributed">
+        <div class="footer-right">
+          <a href="#">
+            <i class="fa fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i class="fa fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i class="fa fa-linkedin"></i>
+          </a>
+          <a href="#">
+            <i class="fa fa-github"></i>
+          </a>
+        </div>
+
+        <div class="footer-left">
+          <p class="footer-links">
+            <a class="link-1" href="#">
+              Home
+            </a>
+
+            <a href="#">About</a>
+
+            <a href="#">Contact</a>
+          </p>
+          <p>
+            @mjshubham21 Copyright &copy; {new Date().getFullYear()} All Rights
+            Reserved.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
