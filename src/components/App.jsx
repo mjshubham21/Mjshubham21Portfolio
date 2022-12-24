d8d0f138c29bb777d852346f4b6feb3d96aa6bc7
Shubham R Pawar <mjshubhampawar21@gmail.com>
2022-12-24 16:39:37 +0530

    Contact Form 1st iteration.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,7 @@ import Intro from "./Intro";
 import Portfolio from "./Portfolio";
 import Contact from "./Contact";
 import Skills from "./Skills";
-import skills from "./Skill";
 import Footer from "./Footer";
-
-// function addSkill(skill) {
-//   return (
-//     <Skills
-//       key={skill.id}
-//       id={skill.id}
-//       icon={skill.icon}
-//       iconName={skill.iconName}
-//     />
-//   );
-// }
 
 function App() {
   return (
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Contact() {
-  return <div></div>;
+  return (
+    <>
+      <div class="wrapper">
+        <form class="form">
+          <div class="pageTitle title">Sign Up </div>
+          <div class="secondaryTitle title">
+            Please fill this form to sign up.
+          </div>
+          <input type="text" class="name formEntry" placeholder="Name" />
+          <input type="text" class="email formEntry" placeholder="Email" />
+          <textarea class="message formEntry" placeholder="Message"></textarea>
+          <button class="submit formEntry" onclick="thanks()">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
