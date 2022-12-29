380921ff4e5d0506eddbc77085e0fe3c016f0795
Shubham R Pawar <mjshubhampawar21@gmail.com>
2022-12-29 18:27:36 +0530

    Implemented AOS.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./NavBar";
 import Intro from "./Intro";
 import Portfolio from "./Portfolio";
 import Contact from "./Contact";
 import Skills from "./Skills";
 import Footer from "./Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    });
+  }, []);
   return (
     <>
       <NavBar />
