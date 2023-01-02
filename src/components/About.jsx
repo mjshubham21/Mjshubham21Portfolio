1e3d6bbfcc8fe6c9ef6d3f5898836f4549fc3e93
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-01-02 18:09:18 +0530

    React Router err, no path #about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,20 @@ import React from "react";
 function About() {
   return (
     <>
-      <section id="about"></section>
+      <section id="about">
+        <div className="container">
+          <p>
+            On this website's main page you will find everything
+            lifestyle-related - fashion, food, beauty, home decor, and more. But
+            it's the About section that introduces you to the team that makes
+            this website an endless source of inspiration. The page introduces
+            founder Emily Schuman, as well as her blog, books, and fashion
+            collection. Want to stay in touch? No problem - the page also
+            features useful links to her social media pages, as well as her
+            online shop.
+          </p>
+        </div>
+      </section>
     </>
   );
 }
