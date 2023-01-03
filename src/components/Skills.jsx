8d191ff93785a6849012ac614a3a8590fec2aa0c
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-01-03 17:02:06 +0530

    Skill section Grid responsiveness try 1.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,44 +5,44 @@ function Skills() {
   return (
     <>
       <section className="skills">
-        <div className="card">
-          <div data-aos="zoom-out-down" className="content">
+        <div data-aos="zoom-out-down" className="card">
+          <div className="content">
             <img className="skillIcon" src="/assets/html5.svg" alt="Icon" />
             <p className="skillName">HTML</p>
           </div>
-          <div data-aos="zoom-out-down" className="content">
+          <div className="content">
             <img className="skillIcon" src="/assets/css3.svg" alt="Icon" />
             <p className="skillName">CSS3</p>
           </div>
-          <div data-aos="zoom-out-down" className="content">
+          <div className="content">
             <img className="skillIcon" src="/assets/js.svg" alt="Icon" />
             <p className="skillName">JavaScript</p>
           </div>
-          <div data-aos="zoom-out-down" className="content">
+          <div className="content">
             <img className="skillIcon" src="/assets/react.svg" alt="Icon" />
             <p className="skillName">ReactJs</p>
           </div>
-          <div data-aos="zoom-out-down" className="content">
+          <div className="content">
             <img className="skillIcon" src="/assets/mongodb.svg" alt="Icon" />
             <p className="skillName">MongoDB</p>
           </div>
-          <div data-aos="zoom-out-down" className="content">
+          <div className="content">
             <img className="skillIcon" src="/assets/express.svg" alt="Icon" />
             <p className="skillName">ExpressJs</p>
           </div>
-          <div data-aos="zoom-out-down" className="content">
+          <div className="content">
             <img className="skillIcon" src="/assets/github.svg" alt="Icon" />
             <p className="skillName">GitHub</p>
           </div>
-          <div data-aos="zoom-out-down" className="content">
+          <div className="content">
             <img className="skillIcon" src="/assets/node.svg" alt="Icon" />
             <p className="skillName">NodeJs</p>
           </div>
-          <div data-aos="zoom-out-down" className="content">
+          <div className="content">
             <img className="skillIcon" src="/assets/password.svg" alt="Icon" />
             <p className="skillName">Authentication</p>
           </div>
-          <div data-aos="zoom-out-down" className="content">
+          <div className="content">
             <img className="skillIcon" src="/assets/api.svg" alt="Icon" />
             <p className="skillName">API</p>
           </div>
