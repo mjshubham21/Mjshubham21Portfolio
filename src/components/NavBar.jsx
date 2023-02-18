14181cee5a31b33a42b551e914d6afcea9ceaca9
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-02-18 19:18:23 +0530

    useState to check is nav is open or not.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,12 +5,6 @@ import { FaBars, FaTimes } from "react-icons/fa";
 
 function NavBar() {
   const navRef = useRef();
-  // const showNavBar = () => {
-  //   navRef.current.classList.toggle("responsive_nav");
-  //   navRef.current.classList.toggle("responsive_nav");
-  //   navRef.current.classList.toggle("nav-closed");
-  //   navRef.current.classList.toggle("nav-open");
-  // };
   const [isOpen, setIsOpen] = useState(false);
   const showNavBar = () => {
     setIsOpen(!isOpen);
@@ -43,11 +37,11 @@ function NavBar() {
           </ul>
         </div>
       </nav>
-      <button className="nav-btn nav-close-btn" onClick={showNavBar}>
-        <FaTimes />
-      </button>
       <button className="nav-btn" onClick={showNavBar}>
         <FaBars />
+      </button>
+      <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+        <FaTimes />
       </button>
     </>
   );
