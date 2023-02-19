5eead5173ab74c8cbea5429683bb58811f2c5250
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-02-19 18:23:57 +0530

    Deleted the comments.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,39 +11,6 @@ function NavBar() {
   };
   return (
     <>
-      {/* <nav
-        className={`mobile-nav ${isOpen ? "responsive_nav" : ""}`}
-        ref={navRef}
-      >
-        <div className="itemL">
-          <Link to="#">mjshubham21</Link>
-        </div>
-        <div className="itemR">
-          <ul>
-            <li>
-              <Link to="#">Home</Link>
-            </li>
-            <li>
-              <Link to="#projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="#contact">Contact Me</Link>
-            </li>
-            <li>
-              <Link to="/about" target="_blank">
-                About Me
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <button className="nav-btn" onClick={showNavBar}>
-        <FaBars />
-      </button>
-      <button className="nav-btn nav-close-btn" onClick={showNavBar}>
-        <FaTimes />
-      </button>
-    </> */}
       <nav
         className={`mobile-nav ${isOpen ? "responsive_nav" : ""}`}
         ref={navRef}
