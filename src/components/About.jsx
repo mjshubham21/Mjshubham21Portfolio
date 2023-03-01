a59f4d5978396739bc06273db766ee424bf2b006
Shubham R Pawar <mjshubhampawar21@gmail.com>
2023-03-01 20:23:24 +0530

    450px media query basic.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,14 @@ function About() {
         <h3 className="about-h3">About Me</h3>
         <div className="about">
           <img className="about-img" src={hero2} alt="hero 2" />
-          Hey I'm a Web Developer mainly foucused on Front End and polishing
-          Backend skills... <br />I am currently pursuing Master's in Computer
-          Science at Pune University. I finished leaning basic MERN stack
-          development in just 73 days, I've documented my learning jounrey on
-          Linkedin, do checkout...
+          <p>
+            {" "}
+            Hey I'm a Web Developer mainly foucused on Front End and polishing
+            Backend skills... <br />I am currently pursuing Master's in Computer
+            Science at Pune University. I finished leaning basic MERN stack
+            development in just 73 days, I've documented my learning jounrey on
+            Linkedin, do checkout...
+          </p>
         </div>
       </section>
     </>
